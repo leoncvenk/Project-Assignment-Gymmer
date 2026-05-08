48d0cf36e28fe26de2e1133dab6cdefb047319b0
Prlekk <marinic.sola@gmail.com>
2026-05-09 00:22:50 +0200

    feat(RAI/backend): integrate mongodb into application startup

--- RAI/backend/app/main.py
+++ RAI/backend/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+
 from app.routes.foods import router as foods_router
+from app.core.database import connect_to_mongo, close_mongo_connection
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await connect_to_mongo()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await close_mongo_connection()
 
 app.include_router(foods_router)
