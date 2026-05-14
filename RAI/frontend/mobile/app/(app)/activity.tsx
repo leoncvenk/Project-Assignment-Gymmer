02755f60a3d85d23e7cdd69bcb030dc453ca6821
Prlekk <marinic.sola@gmail.com>
2026-05-14 02:44:42 +0200

    chore(NPO/mobile): add placeholder route screens

---
+++ RAI/frontend/mobile/app/(app)/activity.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function DashboardScreen() {
+  return (
+    <View className="bg-background flex-1 items-center justify-center">
+      <Text className="text-text text-2xl font-bold">Activity</Text>
+    </View>
+  );
+}
