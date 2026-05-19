20845d71c6983c38368ca3bc6f8e5272f541c466
leoncvenk <leon.cvenk2@gmail.com>
2026-05-19 22:23:35 +0200

    KAN-41 feat(RAI/backend/scraper): setup okusno scraper structure

---
+++ RAI/backend/scraper/okusno_scraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://www.okusno.si"
+START_URL = "https://www.okusno.si/category/meso/"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+def dobi_recepte():
+    pass
+
+if __name__ == "__main__":
+    print("Okusno scraper")
