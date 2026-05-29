61a843244930ab6969dc094d3ff6dcea4cb78edf
Zan Emersic <z.emersic@gmail.com>
2026-05-29 15:19:39 +0200

    feat(ORV/models/food): Added new smarter model for food recognition

--- ORV/training/train.py
+++ ORV/training/train.py
@@ -1,24 +1,23 @@
 from ultralytics import YOLO
 
 def main():
-    # Naloži predtrenirani model YOLOv8s. 
-    # Ta model je že bil treniran na velikem naboru podatkov in bo služil
-    # kot dobra osnova za nadaljnje učenje na naboru slik hrane.
-    model = YOLO("yolov8s.pt")
+    # Load the pre-trained YOLO11 Medium model. 
+    model = YOLO("yolo11m.pt")
 
-    # proces učenja z optimiziranimi nastavitvami
+    # Training process with optimized settings
     results = model.train(
-        data="Complete-Food-1/data.yaml",
-        epochs=100,             # Povečamo število epoh. Model bo sam ustavil učenje (early stopping), če napredka ne bo več.
+        data="Food-1/data.yaml",
+        epochs=150,
         imgsz=640,              
-        batch=16,               # Če med učenjem dobiš napako 'Out of Memory', zmanjšaj to vrednost na 8.
-        workers=8,              # Uporaba več jeder procesorja za hitrejše nalaganje 47k slik.
-        device='0',           # 'cpu' Uporaba CPU namesto GPU. Če imaš dostop do GPU, spremeni na 'cuda'.
+        batch=8,
+        workers=0,                  
+        device='0',                 # Ensures it utilizes dedicated GPU
         project="runs/detect",  
-        name="training_food_v2" 
+        name="training_food_yolo11m", 
+        patience=25                 # The model will automatically stop if accuracy doesn't improve for 25 epochs
     )
 
-    print("Learning Complete. Best results are in runs/detect/training_food_v2/weights/best.pt")
+    print("Learning Complete. Results:", results)
 
 if __name__ == '__main__':
     main()
