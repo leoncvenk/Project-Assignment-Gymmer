fc1fb8b5abcf22a06d43c45830459666a14294b5
Prlekk <marinic.sola@gmail.com>
2026-06-05 21:57:14 +0200

    feat(RAI/backend): add ORV API configuration

--- RAI/backend/app/core/config.py
+++ RAI/backend/app/core/config.py
@@ -4,6 +4,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MONGO_URI: str
     DB_NAME: str = "gymmer"
+    TEST_DB_NAME: str = "gymmer_test"
+    
+    ORV_API_URL: str = "http://127.0.0.1:8001"
 
     class Config:
         env_file = ".env"
